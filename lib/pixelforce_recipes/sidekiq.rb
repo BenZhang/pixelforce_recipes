256cfcd611f9ad1779c240708eba0b2f6ee19766
Ben Zhang <bzbnhang@gmail.com>
2016-09-13 15:00:35 +0930

    fix sidekiq

--- lib/pixelforce_recipes/sidekiq.rb
+++ lib/pixelforce_recipes/sidekiq.rb
@@ -1,19 +1,23 @@
-namespace :sidekiq do
-  desc "Install sidekiq"
+if Capistrano::Configuration.instance(false)
+  Capistrano::Configuration.instance(true).load do |instance|
+    namespace :sidekiq do
+      desc "Install sidekiq"
 
-  desc "Setup sidekiq configuration for this application"
-  task :setup, roles: :sidekiq do
-    template "sidekiq_init.erb", "/tmp/sidekiq"
-    run "#{sudo} mv /tmp/sidekiq /etc/init.d/sidekiq"
-    run "#{sudo} chmod +x /etc/init.d/sidekiq"
-    run "#{sudo} update-rc.d sidekiq defaults"
-  end
-  # after "deploy:setup", "nginx:setup"
-  
-  %w[start stop restart].each do |command|
-    desc "#{command} sidekiq"
-    task command do
-      run "/etc/init.d/sidekiq #{command}"
+      desc "Setup sidekiq configuration for this application"
+      task :setup, roles: :sidekiq do
+        template "sidekiq_init.erb", "/tmp/sidekiq"
+        run "#{sudo} mv /tmp/sidekiq /etc/init.d/sidekiq"
+        run "#{sudo} chmod +x /etc/init.d/sidekiq"
+        run "#{sudo} update-rc.d sidekiq defaults"
+      end
+      # after "deploy:setup", "nginx:setup"
+      
+      %w[start stop restart].each do |command|
+        desc "#{command} sidekiq"
+        task command do
+          run "/etc/init.d/sidekiq #{command}"
+        end
+      end
     end
   end
 end
