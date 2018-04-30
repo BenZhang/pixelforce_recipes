a085ea22a2bc6c776bf107bc41395d018a209659
BenZhang <bzbnhang@gmail.com>
2018-04-30 14:49:41 +0930

    fix bugs

--- lib/pixelforce_recipes.rb
+++ lib/pixelforce_recipes.rb
@@ -1,4 +1,8 @@
 require "pixelforce_recipes/version"
 require "capistrano"
 
-require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/*.rb")
+require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/base")
+require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/unicorn")
+require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/sidekiq")
+require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/puma")
+require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/logrotate")
