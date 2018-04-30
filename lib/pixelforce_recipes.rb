093b5e878a192c3cf765384904c7c0d684e5059e
BenZhang <bzbnhang@gmail.com>
2018-04-30 14:44:46 +0930

    bump version

--- lib/pixelforce_recipes.rb
+++ lib/pixelforce_recipes.rb
@@ -1,7 +1,4 @@
 require "pixelforce_recipes/version"
 require "capistrano"
 
-require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/base")
-require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/unicorn")
-require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/sidekiq")
-require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/puma")
+require File.expand_path("#{File.dirname(__FILE__)}/pixelforce_recipes/*.rb")
