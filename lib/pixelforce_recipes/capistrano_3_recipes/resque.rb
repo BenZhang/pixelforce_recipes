bb5af7b29237b3e83b5174f97921aa12ff36fb50
chong yang <iamyangchong@gmail.com>
2020-02-19 15:06:18 +1030

    Update the capistrano3 task with sysvinit again (#11)
    * feat(supervisor):change capistrano3 recipes with supervisor
    
    * feat(readme):update the README doc for the new feature
    
    * fix(template):missing a ) in the end
    
    * feat(task):rename puma as pf_puma and add resque_scheduler
    
    * feat(version):bump up the version into 1.7
    
    * fix(template):correct the log path of sidekiq
    
    * feat(capistrano3):add sysvinit support back
    
    * doc(README):update the readme file for new feature

--- lib/pixelforce_recipes/capistrano_3_recipes/resque.rb
+++ lib/pixelforce_recipes/capistrano_3_recipes/resque.rb
@@ -2,20 +2,41 @@ namespace :resque do
   desc "Install resque"
 
   desc "Setup resque configuration for this application"
-  task :setup do
-    on roles(:resque) do
-      template "resque_supervisor.erb", "/tmp/resque"
-      sudo "mv /tmp/resque /etc/supervisor/conf.d/resque"
-      sudo "supervisorctl reread"
-      sudo "supervisorctl update" # it will auto start the application
+  namespace :sysvinit do
+    task :setup do
+      on roles(:resque) do
+        template "resque_init.erb", "/tmp/resque"
+        sudo "mv /tmp/resque /etc/init.d/resque"
+        sudo "chmod +x /etc/init.d/resque"
+        sudo "update-rc.d resque defaults"
+      end
+    end
+
+    %w[start stop restart].each do |command|
+      desc "#{command} resque"
+      task command do
+        on roles(:resque) do
+          execute "/etc/init.d/resque #{command}"
+        end
+      end
     end
   end
-
-  %w[start stop restart].each do |command|
-    desc "#{command} resque"
-    task command do
+  namespace :supervisor do
+    task :setup do
       on roles(:resque) do
-        execute "supervisorctl #{command} resque"
+        template "resque_supervisor.erb", "/tmp/resque"
+        sudo "mv /tmp/resque /etc/supervisor/conf.d/resque"
+        sudo "supervisorctl reread"
+        sudo "supervisorctl update" # it will auto start the application
+      end
+    end
+
+    %w[start stop restart].each do |command|
+      desc "#{command} resque"
+      task command do
+        on roles(:resque) do
+          execute "supervisorctl #{command} resque"
+        end
       end
     end
   end
